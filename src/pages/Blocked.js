d2023eb741e93bd33ce46a196344ef306875042d
vuggy17 <vuggy17@gmail.com>
2022-03-09 11:02:15 +0700

    update webpack.config

--- src/pages/Blocked.js
+++ src/pages/Blocked.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 export class Blocked extends Component {
+  justaddsomecode = "w";
   render() {
     return <div>You have no access permision</div>;
   }
